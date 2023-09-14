2194d0fc30b05303dae2498a4b3037def2dbc16f
Thapakorn11 <Kon71380@gmail.com>
2023-09-14 22:00:25 +0700

    first commit

---
+++ src/main/java/com/mycompany/labgui1/LabGUI1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labgui1;
+
+/**
+ *
+ * @author tanan
+ */
+public class LabGUI1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
